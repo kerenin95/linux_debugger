2cfd1bd8ccf145591e65674a3fa013330f84a4b1
kerenin95 <weberbencs@gmail.com>
2021-03-26 23:02:58 -0400

    init class

--- ldbg.cpp
+++ ldbg.cpp
@@ -95,3 +95,14 @@ void debugger::print_backtrace() {
 		return_address = read_memory(frame_pointer + 8);
 	}
 }
+
+symbol_type to_symbol_type(elf::stt sym) {
+	switch (sym) {
+	case elf::stt::notype: return symbol_type::notype;
+	case elf::stt::object: return symbol_type::object;
+	case elf::stt::func: return symbol_type::func;
+	case elf::stt::section: return symbol_type::section;
+	case elf::stt::file: return symbol_type::file;
+	default: return symbol_type::notype;
+	}
+};
